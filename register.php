e1ee91d3738b148c9b5a95b0cf12688b68a714a5
maneesh1318 <maneesh1318@gmail.com>
2014-07-08 23:03:19 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,20 @@
+<table border="0">
+<tr><td>Username:</td><td><input type="text" id="r_input_1" /></td><td><button onclick="user_name_avail()">Check Availability</button></td></tr>
+<tr><td></td><td id="r_err_msg_1"></td></tr>
+<tr><td>First Name:</td><td><input type="text" id="r_input_2" /></td></tr>
+<tr><td></td><td id="r_err_msg_2"></td></tr>
+<tr><td>Last Name:</td><td><input type="text" id="r_input_3" /></td></tr>
+<tr><td></td><td id="r_err_msg_3"></td></tr>
+<tr><td>Gender</td><td><input type="radio" name="gender" value="male" id="r1">Male
+<input type="radio" name="gender" value="female" id="r2">Female</td></tr>
+<tr><td></td><td id="r_err_msg_4"></td></tr>
+<tr><td>Password:</td><td><input type="password" id="r_input_4" /></td></tr>
+<tr><td></td><td id="r_err_msg_5"></td></tr>
+<tr><td>Confirm Password:</td><td><input type="password" id="r_input_5" /></td></tr>
+<tr><td></td><td id="r_err_msg_6"></td></tr>
+<tr><td>Email ID</td><td><input type="text" id="r_input_6" /></td></tr>
+<tr><td></td><td id="r_err_msg_7"></td></tr>
+<tr><td>Mobile No:</td><td><input type="text" id="r_input_7" /></td></tr>
+<tr><td></td><td id="r_err_msg_8"></td></tr>
+<tr><td><button id="reg_Btn" onclick="register()" disabled>Register</button></td></tr>
+</table>
